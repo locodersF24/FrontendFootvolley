b11ff0c50db49ac144f28531d4e8cd98fa89bd75
JosefineChristine <joro0003@stud.kea.dk>
2025-05-06 10:33:33 +0200

    lavet html til login, mangler js

---
+++ login/script.js
@@ -0,0 +1,17 @@
+document.querySelector("form").addEventListener("submit", login);
+
+async function login(event) { // The async keyword in JavaScript is used to declare a function as asynchronous, meaning that the function will perform operations that can be executed in parallel without blocking the rest of the code from running.
+    event.preventDefault(); // Prevent page reload
+
+    // Get the latest values from input fields
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    response.ok
+
+
+
+    //TODO: lave login-funktion i javascript
+
+
+}
